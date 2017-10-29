3de4bb2d618ea92800ab8dd01c6c679e45091c9f
Akash Goswami <akashneil96@gmail.com>
2017-10-29 11:42:01 +0000

    Initial commit
    Added project files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Credit Card Workout
+Porticode 2.0 2017 Project submission
+
+## About
+Website that takes a users credit card balance, interest rate and monhtly amount they wish to pay to calculate how long it would take them to pay their debit off as well as showing them how much interest they have accumulated. This was made within 24 hours for the Porticode Hackathon 2017
+
+## Made by
+[Akash Goswami](https://twitter.com/Skyther_)
+Alice Hinton-Jones
+
+## Build with
+[Bootstrap](https://getbootstrap.com)
+
+### Disclaimer
+Calculations are not 100% accurate. This tool should only be used to give you rough estimations
---
+++ js/ccw.js
@@ -0,0 +1,75 @@
+function fixedMonthlyAmountToPayFixed(balance, monthlyRate, string) {
+    // main calculation
+    var temp = balance;
+    balance = balance * (1 + monthlyRate);
+    accInterest = accInterest + (balance - temp);
+
+    if (balance >= monthlyAmountToPayFixed) {
+        balance = balance - monthlyAmountToPayFixed;
+        balance = (Math.round(balance * 100) / 100).toFixed(2);
+        //console.log('Month ' + monthCounter);
+        //balance = Math.round(balance * 100) / 100
+        //console.log('Pay £' + monthlyAmountToPayFixed);
+        //console.log('Remaining balance: £' + balance);
+        //console.log('===========================');
+        monthCounter++;
+        return fixedMonthlyAmountToPayFixed(balance,monthlyRate,string);
+    } else {
+        string += 'Overall you have accumulated £' + accInterest.toFixed(2)  + ' of interest.\n';
+        string += 'To pay off your debt, it will take you ' + monthCounter + ' months.\n';
+        //string += '=================================================\n';
+        return string;
+    };
+};
+
+function fixedMonths(){
+    var temp = balance;
+    balance = balance * (1 + monthlyRate);
+    accInterest = accInterest + (balance - temp);
+
+    if (balance >= monthlyRatePayments) {
+        balance = balance - monthlyRatePayments;
+        balance = (Math.round(balance * 100) / 100).toFixed(2);
+        monthCounter--;
+        fixedMonths();
+    } else {
+        // console.log('Overall you have accumulated £' + accInterest.toFixed(2)  + ' of interest.');
+        // console.log('To pay off your debt in ' + monthNumber + ' months, you will need to make monthly repayments of £' + monthlyRatePayments.toFixed(2));
+        // console.log('========================================================================================');
+    };
+
+};
+
+// How much you owe
+var balance = 1739.80;
+// get whole number and convert to decimal (eg. rate is in the format 23%)
+var yearlyRate = 22.9;
+// The interest that has been accumulated
+var accInterest = 0;
+
+// Method 1
+// Has to be more than 2% of the balance to work
+var monthlyAmountToPayFixed = 188.21;
+var monthCounter = 1;
+
+// Method 2
+// Input no of months
+var monthNumber = 18;
+var monthlyRatePayments = balance / monthNumber;
+
+// Change Annual rate of interest to monthlyRate
+var monthlyRate = Math.pow(yearlyRate, 1/12);
+monthlyRate = monthlyRate / 100;
+//console.log(monthlyRate);
+monthlyRate = Math.round(monthlyRate * 10000) / 10000;
+//console.log(monthlyRate);
+
+// if (monthlyAmountToPayFixed < (balance/100)*2){
+//     console.log("ERROR - monthlyRate payments can not be less that 2% of your balance");
+// } else {
+//     // For fixed monthlyRate amount to pay
+//     fixedMonthlyAmountToPayFixed();
+
+//     // For fexiable monthlyRate amount to pay
+//     fixedMonths()
+// };
